aa5ba6da24462b2ac71ee84a9d8d1f884ba36f9d
nomvula <nzshabangu95@gmail.com>
2023-09-14 11:13:50 +0200

    Add project files.

---
+++ Constructors cars/Cars.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_cars
+{
+    class Cars
+    {
+        public string model;
+        public string color;
+        public int year;
+
+        public Cars(string aModel, string aColor, int aYear)
+        {
+            model = aModel;
+            color = aColor;
+            year = aYear;
+        }
+    }
+}
---
+++ Constructors cars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors_cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cars CarDes = new Cars("bmw m4","yellow", 2020);
+            Cars CarDes1 = new Cars("benz c63","black", 2021);
+
+            Console.WriteLine(CarDes.model);
+            Console.WriteLine(CarDes.color);
+            Console.WriteLine(CarDes.year);
+            Console.ReadLine();
+
+        }
+    }
+}
